060beefe9c4654098dcc2d0c8a52adde62d74a33
madelinegit <120407458+madelinegit@users.noreply.github.com>
2023-12-30 20:43:51 -0600

    renamed default component, conducted just exports, added clicker button with a class in classcomp.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
+import FunctionalCompNickname from './components/FunctionalComp';
+//first functional comp imported here
+//import ClassCompNickname from './components/ClassComp';
+import {ClassComp, ClickCountButton} from './components/ClassComp';
 
 function App() {
   return (
@@ -7,19 +11,27 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <code>src/App.js</code> seems to be in charge here...<br></br>
+          *All HTML Tags must be enclosed within a DIV Tag
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://linkedin.com/in/madelinemarie"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Madeline's LinkedIn
         </a>
+        <p><ClickCountButton></ClickCountButton></p>
+        <h2><FunctionalCompNickname></FunctionalCompNickname></h2>
+        <h2><ClassComp></ClassComp></h2>
       </header>
     </div>
   );
 }
+//first functional component displayed here with default import
+//first class component displayed here with default import
+
+
 
 export default App;
--- src/components/ClassComp.js
+++ src/components/ClassComp.js
@@ -1,7 +1,28 @@
 import React from "react";
 
-class ClassComp extends React.Component{
+export class ClassComp extends React.Component{
     render(){
         return <p> My First Class Component</p>
+    }
+}
+
+//adding state to a class component
+export class ClickCountButton extends React.Component {
+    state = {
+        count: 0
+    };
+
+    onClick() {
+        this.setState(state => ({
+            count: state.count + 1
+        }));
+    }
+
+    render() {
+        return (
+            <button onClick={this.onClick.bind(this)}>
+                You Clicked {this.state.count} times.
+            </button>
+        );
     }
 }
