20a4bc45d656c33bff43ada0c2d89e40c5e7b4d2
madelinegit <120407458+madelinegit@users.noreply.github.com>
2023-12-30 19:49:21 -0600

    altered html, created component files & docs from terminal, created first functional component

---
+++ src/components/FunctionalComp.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function FunctionalComp(){
+    return <p>This is my first Functional Component</p>
+}
+
+export default FunctionalComp
+//only one default export per file!
+//imported in App.js
