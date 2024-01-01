2c46f1955c5fd895a6f05dd4117230828a41e95c
madelinegit <120407458+madelinegit@users.noreply.github.com>
2023-12-31 19:31:48 -0600

    image was contorted, fixed it in the inline style, added display flex to avatar in inline style, image is once again contorted.. progress

--- src/components/ProfileFuncComp.js
+++ src/components/ProfileFuncComp.js
@@ -2,27 +2,39 @@ import React from "react";
 
 //from react documentation
 const user = {
-    name: 'Hedy Lamarr',
-    imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
-    imageSize: 90,
+    name: 'Madeline Gall',
+    imageUrl: 'avatar.jpg',
+    imageSize: 120,
+    // unit of measurement??^
 };
+
 
 export default function Profile() {
     // const containerStyle ={
     //     // textAlign: 'left',
     // }
+const containerStyle = {
+    display: 'flex',  //should have made name & pic go next to each other
+    textDecoration: 'none', // remove underline
+};
+
 return (
-    <>
-    <h1>{user.name}</h1>
-    <img
-        className="avatar"
-        src={user.imageUrl}
-        alt={'Photo of ' + user.name}
-        style={{
-        width: user.imageSize,
-        height: user.imageSize
-        }}
-    />
-</>
+    <div style ={containerStyle}>
+    <a href="https://linkedin.com/in/madelinemarie" className="Avatar">
+        <h1>{user.name}</h1>
+        {/* <container ClassName="AvatarPic"> */}
+    </a>
+        <img  ClassName="AvatarPic" src={user.imageUrl}
+            alt={'Photo of ' + user.name}
+            style={{
+            width: user.imageSize,
+            // height: user.imageSize,
+            //removed to fix object fit
+            }}
+        >
+        </img>
+
+        {/* </container> */}
+    </div>
 );
 }
