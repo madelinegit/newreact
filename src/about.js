0be502ac335caceec8611ecca8dc71bf819a432c
madelinegit <120407458+madelinegit@users.noreply.github.com>
2024-01-02 12:16:38 -0600

    created about page, found lots of problems in router dom, broke code, troubleshooted, going to revert to previous version after recording this commit

---
+++ src/about.js
@@ -0,0 +1,29 @@
+// //import logo from './logo.svg';
+// import './App.css';
+// import FunctionalCompNickname from './components/DefaultFuncComp';
+// //imported from default export
+// //first functional comp imported here
+// //import ClassCompNickname from './components/ClassComp';
+// import {ClassComp, ClickCountButton} from './components/ClassComp';
+// //normal import
+// import { ButtonClickerFunction} from './components/FuncComp'
+// //normal import
+// import Profile from './components/ProfileFuncComp';
+// //normal import only one default export is allowed
+// import { Component } from 'react';
+// import { TopRight } from './components/TopRight';
+
+
+
+// function About() {
+//     return (
+//         <div className="App">
+//             <header className="App-header">
+//             <Profile className="Profile"></Profile>
+//             <a href = "https://github.com/madelinegit/newreact"><img src={logo} className="App-logo" alt="logo" /></a>
+
+//     </header>
+//     </div>
+//     );
+// };
+// export default About;
